77c3773ebb9da85add9d9125d2e64b96599ff49a
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-16 17:45:49 +0900

    add enum field

--- src/main/java/com/example/uorders/domain/CartMenu.java
+++ src/main/java/com/example/uorders/domain/CartMenu.java
@@ -1,5 +1,6 @@
 package com.example.uorders.domain;
 
+import com.example.uorders.Service.MenuService;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,5 +24,45 @@ public class CartMenu {
 
     private int count;
     private int orderPrice;
+
+    @Enumerated(EnumType.STRING)
+    private MenuTemperature menuTemperature; // HOT, ICED
+
+    @Enumerated(EnumType.STRING)
+    private MenuSize menuSize; // SMALL, REGULAR, LARGE
+
+    @Enumerated(EnumType.STRING)
+    private MenuTakeType menuTakeType; // HERE, TO_GO
+
+    //==연관관계 메서드==//
+    public void setCart(Cart cart) {
+        this.cart = cart;
+        cart.getCartMenus().add(this);
+    }
+
+    //==생성 메서드//
+    public static CartMenu createCartMenu(Menu menu, int orderPrice, int count, MenuTemperature menuTemperature, MenuSize menuSize, MenuTakeType menuTakeType) {
+        CartMenu cartMenu = new CartMenu();
+        cartMenu.setMenu(menu);
+        cartMenu.setOrderPrice(orderPrice);
+        cartMenu.setCount(count);
+        cartMenu.setMenuTemperature(menuTemperature);
+        cartMenu.setMenuSize(menuSize);
+        cartMenu.setMenuTakeType(menuTakeType);
+
+        return cartMenu;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     *  장바구니 메뉴 삭제
+     */
+    //public void deleteCartMenu() { }
+
+    //==조회 로직==//
+    /**
+     * 전체 장바구니 가격 조회
+     */
+    public int getTotalPrice() { return getOrderPrice() * getCount(); }
 
 }
