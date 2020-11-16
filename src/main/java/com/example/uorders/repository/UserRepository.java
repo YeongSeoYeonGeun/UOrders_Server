a723b8633a234157c836c2f5c9db9670c199e27b
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-16 17:47:53 +0900

    create new user repository

--- src/main/java/com/example/uorders/repository/UserRepository.java
+++ src/main/java/com/example/uorders/repository/UserRepository.java
@@ -1,27 +1,10 @@
 package com.example.uorders.repository;
 
 import com.example.uorders.domain.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class UserRepository {
-
-    @PersistenceContext
-    private final EntityManager em;
-
-    public void save(User user) { em.persist(user); }
-
-    public User findOne(Long id) { return em.find(User.class,id); }
-
-    public List<User> findAll() {
-        return em.createQuery("select u from User u", User.class)
-                .getResultList();
-    }
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
 }
