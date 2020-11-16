aaadfec69b64b440c414a44b8b242959fdf98b01
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-16 17:47:17 +0900

    create cafe repository

--- src/main/java/com/example/uorders/repository/CafeRepository.java
+++ src/main/java/com/example/uorders/repository/CafeRepository.java
@@ -1,29 +1,11 @@
 package com.example.uorders.repository;
 
 import com.example.uorders.domain.Cafe;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class CafeRepository {
+public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
-    @PersistenceContext
-    private final EntityManager em;
-
-    public void save(Cafe cafe) { em.persist(cafe); }
-
-    public Cafe findOne(Long id) { return em.find(Cafe.class, id); }
-
-    public List<Cafe> findAll(){
-        return em.createQuery("select c from Cafe c", Cafe.class)
-                .getResultList();
-    }
-
-    // 없어도 될 듯 ?
-    // public List<Cafe> findByName(String name)
+    List<Cafe> findByName(String name);
 }
