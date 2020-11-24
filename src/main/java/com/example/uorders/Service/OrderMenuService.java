884f95db38c56022e1723842587bf84d6561ecb9
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-11-25 01:56:12 +0900

    create create_order api

--- src/main/java/com/example/uorders/Service/OrderMenuService.java
+++ src/main/java/com/example/uorders/Service/OrderMenuService.java
@@ -1,10 +1,16 @@
 package com.example.uorders.Service;
 
+import com.example.uorders.domain.Cart;
+import com.example.uorders.domain.CartMenu;
+import com.example.uorders.domain.Order;
 import com.example.uorders.domain.OrderMenu;
 import com.example.uorders.repository.OrderMenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,4 +21,18 @@ public class OrderMenuService {
 
     @Transactional
     public void saveOrderMenu(OrderMenu orderMenu) { orderMenuRepository.save(orderMenu); }
+
+    @Transactional
+    public Set<OrderMenu> createOrderMenus(Cart cart) {
+
+        Set<OrderMenu> orderMenus = new HashSet<>();
+        Set<CartMenu> cartMenus = cart.getCartMenus();
+
+        for(CartMenu cartMenu: cartMenus) {
+            OrderMenu orderMenu = OrderMenu.createOrderMenu(cartMenu.getMenu(), cartMenu.getOrderPrice(), cartMenu.getCount());
+            orderMenus.add(orderMenu);
+        }
+
+        return orderMenus;
+    }
 }
