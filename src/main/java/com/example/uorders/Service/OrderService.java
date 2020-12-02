3a02faeafcc9a5e6c6bf70ce263eab1e8040dad4
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-12-03 03:13:35 +0900

    feat: #20 결제 구현

--- src/main/java/com/example/uorders/Service/OrderService.java
+++ src/main/java/com/example/uorders/Service/OrderService.java
@@ -1,12 +1,27 @@
 package com.example.uorders.Service;
 
+import com.example.uorders.api.OrderController;
+import com.example.uorders.api.constants.WeChat;
 import com.example.uorders.domain.*;
 import com.example.uorders.dto.order.AcceptOrderRequest;
+import com.example.uorders.dto.order.PayReqeust;
+import com.example.uorders.dto.order.PayResponse;
 import com.example.uorders.exception.OrderNotFoundException;
 import com.example.uorders.repository.*;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.apache.bcel.util.ClassLoaderRepository;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -72,5 +87,83 @@ public class OrderService {
         order.setStatus(OrderStatus.ACCEPTED);
 
         saveOrder(order);
+    }
+
+    public PayResponse pay(User user) {
+
+        String appid = WeChat.appid;
+        String secret = WeChat.secret;
+        int amount = user.getCart().getTotalPrice();
+        String js_code = user.getCode();
+        String grantType = "authorization_code";
+        String openid = "";
+
+        try {
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectionRequestTimeout(5000);
+            factory.setReadTimeout(5000);
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            String url = "https://api.weixin.qq.com/sns/jscode2session";
+
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+                    .queryParam("appid", appid)
+                    .queryParam("secret", secret)
+                    .queryParam("js_code", js_code)
+                    .queryParam("grant_type", grantType);
+
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+
+            HttpEntity<String> response = restTemplate.exchange(
+                    builder.toUriString(),
+                    HttpMethod.GET,
+                    entity,
+                    String.class);
+
+            String responseBody = response.getBody();
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse( responseBody );
+            JSONObject jsonObj = (JSONObject) obj;
+            openid = (String) jsonObj.get("openid");
+
+            System.out.println("opneiddddddddddddddddddddddddddddddddddddddddddd: " + openid);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectionRequestTimeout(5000);
+            factory.setReadTimeout(5000);
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            PayReqeust payRequestBody = PayReqeust.builder().openId(openid).amount(amount).build();
+
+            String url = "https://open.ifprod.cc/api/v1/shoots/pay";
+
+            ResponseEntity<String> resultMap = restTemplate.postForEntity(url, payRequestBody, String.class);
+
+            String responseBody = resultMap.getBody();
+            System.out.println(responseBody);
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse( responseBody );
+            JSONObject jsonObj = (JSONObject) obj;
+            String timestamp = (String) jsonObj.get("timeStamp");
+            String nonceStr = (String) jsonObj.get("nonceStr");
+            String package_ = (String) jsonObj.get("package");
+            String signType = (String) jsonObj.get("signType");
+            String tId = (String) jsonObj.get("tId");
+            String paySign = (String) jsonObj.get("paySign");
+
+            return new PayResponse(timestamp, nonceStr, package_, signType, tId, paySign);
+
+        } catch (RestClientException | ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
