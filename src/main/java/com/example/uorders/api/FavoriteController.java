58d6a935cd6d2cb545a4e8e6d34298047e4be742
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-12-03 20:32:09 +0900

    Favorite 구조 변경 및 번역 추가

--- src/main/java/com/example/uorders/api/FavoriteController.java
+++ src/main/java/com/example/uorders/api/FavoriteController.java
@@ -10,6 +10,7 @@ import com.example.uorders.domain.Cafe;
 import com.example.uorders.domain.Favorite;
 import com.example.uorders.domain.User;
 import com.example.uorders.dto.favorite.FavoriteDto;
+import com.example.uorders.dto.favorite.createFavoriteRequest;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -28,64 +29,32 @@ public class FavoriteController {
     private final CafeService cafeService;
     private final FavoriteService favoriteService;
 
-    /**
-     *  즐겨찾는 매장 조회
-     */
+    /** 즐겨찾는 매장 조회 */
     @GetMapping
     public ResponseEntity<Message> getFavoriteCafe (@RequestHeader("userIndex") Long id) {
-
         User user = userService.findById(id);
+        List<FavoriteDto.FavoriteCafeDto> response = favoriteService.readFavoriteCafeList(user);
 
-        List<Cafe> favoriteCafeList = userService.findFavoriteCafeList(user);
-
-        List<FavoriteDto.FavoriteCafeDto> favoriteCafeDtoList = new ArrayList<>();
-        for(Cafe cafe: favoriteCafeList) {
-            FavoriteDto.FavoriteCafeDto favoriteCafeDto = FavoriteDto.FavoriteCafeDto.of(cafe);
-            favoriteCafeDtoList.add(favoriteCafeDto);
-        }
-
-        Message message = new Message(StatusCode.OK, ResponseMessage.READ_FAVORITE, favoriteCafeDtoList);
+        Message message = new Message(StatusCode.OK, ResponseMessage.READ_FAVORITE, response);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
-    /**
-     *  즐겨찾는 매장 등록
-     */
+    /** 즐겨찾는 매장 등록 */
     @PostMapping
     public ResponseEntity<Message> createFavoriteCafe (@RequestHeader("userIndex") Long userId, @RequestBody createFavoriteRequest request){
-
         User user = userService.findById(userId);
-        Cafe cafe = cafeService.findById(request.cafeIndex);
-
-        Favorite favorite = Favorite.builder()
-                .user(user)
-                .cafe(cafe)
-                .build();
-
-        favoriteService.saveFavorite(favorite);
+        favoriteService.createFavorite(user, request);
 
         Message message = new Message(StatusCode.OK, ResponseMessage.CREATE_FAVORITE);
         return new ResponseEntity<>(message,null,HttpStatus.OK);
     }
 
-    @Data
-    static class createFavoriteRequest {
-        private Long cafeIndex;
-    }
-
-    /**
-     *  즐겨찾는 매장 등록 해제 (삭제)
-     */
+    /**  즐겨찾는 매장 등록 해제 (삭제) */
     @DeleteMapping
     public ResponseEntity<Message> deleteFavorite(@RequestHeader("userIndex") Long userId, @RequestHeader("cafeIndex") Long cafeId) {
-
-        Favorite favorite = favoriteService.findOne(userId, cafeId).orElse(null);
-        if(favorite == null) {
-            Message message = new Message(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_USER_OR_CAFE);
-            return new ResponseEntity<>(message, null, HttpStatus.BAD_REQUEST);
-        }
-
+        Favorite favorite = favoriteService.findById(userId,cafeId);
         favoriteService.deleteOne(favorite);
+
         Message message = new Message(StatusCode.OK, ResponseMessage.DELETE_FAVORITE);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
