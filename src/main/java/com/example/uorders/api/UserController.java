76986ec06daf832b7cd00961c7cf040417436ed9
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-12-04 00:04:09 +0900

    feat: #51 언어 변경 완료

--- src/main/java/com/example/uorders/api/UserController.java
+++ src/main/java/com/example/uorders/api/UserController.java
@@ -6,8 +6,10 @@ import com.example.uorders.Service.UserService;
 import com.example.uorders.api.constants.Message;
 import com.example.uorders.api.constants.ResponseMessage;
 import com.example.uorders.api.constants.StatusCode;
+import com.example.uorders.domain.User;
 import com.example.uorders.dto.user.CreateUserResponse;
 import com.example.uorders.dto.user.LoginRequest;
+import com.example.uorders.dto.user.UpdateUserLanguageCodeRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +29,16 @@ public class UserController {
     public ResponseEntity<Message> login(@RequestBody LoginRequest request) {
         CreateUserResponse response = userService.login(request);
         Message message = new Message(StatusCode.OK, ResponseMessage.LOGIN_SUCCESS, response);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    /** 언어 코드 변경 */
+    @PutMapping("/language")
+    public ResponseEntity<Message> updateLanguage(@RequestHeader("userIndex") Long userId, @RequestBody UpdateUserLanguageCodeRequest request) {
+        User user = userService.findById(userId);
+        userService.updateLanguageCode(user, request);
+
+        Message message = new Message(StatusCode.OK, ResponseMessage.UPDATE_USER_LANGUAGE);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
